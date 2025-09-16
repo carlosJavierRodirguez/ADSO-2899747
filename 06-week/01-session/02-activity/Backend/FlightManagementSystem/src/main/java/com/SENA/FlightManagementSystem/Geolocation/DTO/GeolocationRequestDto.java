ae228ea67e7fe5cfeddc0eff0bc59471da29455f
carlosJavierRodriguez <carlosjavi1887@gmail.com>
2025-09-16 10:53:55 -0500

    feat(dto): create abstract DTO class and implement in continent for requests with only necessary data

---
+++ 06-week/01-session/02-activity/Backend/FlightManagementSystem/src/main/java/com/SENA/FlightManagementSystem/Geolocation/DTO/GeolocationRequestDto.java
@@ -0,0 +1,11 @@
+package com.SENA.FlightManagementSystem.Geolocation.DTO;
+
+import lombok.Data;
+
+@Data
+public class ContinentRequestDto {
+    private String code;
+    private String name;
+    private String description;
+    private Boolean status;
+}
